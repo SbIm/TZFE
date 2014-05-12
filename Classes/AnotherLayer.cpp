af9d2dcc2f41fcb5e68d673fb9bea81fd1ef2229
song bo <bo_song@pinssible.com>
2014-05-13 02:30:53 +0800

    add a button

---
+++ Classes/AnotherLayer.cpp
@@ -0,0 +1,9 @@
+//
+//  AnotherScene.cpp
+//  TZFE
+//
+//  Created by songbo on 14-5-13.
+//
+//
+
+#include "AnotherScene.h"
---
+++ Classes/AnotherLayer.h
@@ -0,0 +1,14 @@
+//
+//  AnotherScene.h
+//  TZFE
+//
+//  Created by songbo on 14-5-13.
+//
+//
+
+#ifndef __TZFE__AnotherScene__
+#define __TZFE__AnotherScene__
+
+#include <iostream>
+
+#endif /* defined(__TZFE__AnotherScene__) */
